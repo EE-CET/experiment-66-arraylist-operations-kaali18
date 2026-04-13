eb323c74347982ac3ec71a0a02afc2cb7d4c0c23
kaali18 <kasinathkv6@gmail.com>
2026-04-13 23:19:47 +0530

    experiment-66-arraylist-operations

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class ArrayListOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            ArrayList<String> items = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                items.add(scanner.next());
+            }
+            String searchItem = scanner.next();
+            
+            Collections.sort(items);
+            System.out.println("Sorted Items: " + items.toString());
+            
+            if (items.contains(searchItem)) {
+                System.out.println("Found");
+            } else {
+                System.out.println("Not Found");
+            }
+        }
+        scanner.close();
     }
 }
